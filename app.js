0e6bb83db3c1179de014ba785a0927129cdb630c
shijuhong <940104683@qq.com>
2022-06-21 23:52:12 +0800

    基本路由实现

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const blog = require('./routes/blog')
+const user = require('./routes/user')
 
 // error handler
 onerror(app)
@@ -33,8 +33,8 @@ app.use(async (ctx, next) => {
 })
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(user.routes(), user.allowedMethods())
+app.use(blog.routes(), blog.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
