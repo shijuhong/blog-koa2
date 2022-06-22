984b4e2fd6517715cf4b60ba6330ec3e30a0e8c5
shijuhong <940104683@qq.com>
2022-06-22 11:29:57 +0800

    登录中间件

---
+++ middleware/loginCheck.js
@@ -0,0 +1,13 @@
+const { ErrorModel } = require("../model/resModel");
+
+const loginCheck = async (ctx, next) => {
+  if (ctx.session.username) {
+    await next();
+  } else {
+    ctx.body = new ErrorModel("未登录")
+  }
+};
+
+module.exports = {
+  loginCheck,
+};
