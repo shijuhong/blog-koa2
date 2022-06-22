bf43c2930644c8998d531c0a303c3fad745fc0ef
shijuhong <940104683@qq.com>
2022-06-22 09:51:46 +0800

    实现 session 与 redis 连接

--- routes/user.js
+++ routes/user.js
@@ -11,4 +11,15 @@ router.post("/login", async (ctx, next) => {
   };
 });
 
+router.get("/session-test", async (ctx, next) => {
+  if (ctx.session.viewCount == null) {
+    ctx.session.viewCount = 0;
+  }
+  ctx.session.viewCount++;
+  ctx.body = {
+    errno: 0,
+    viewCount: ctx.session.viewCount,
+  };
+});
+
 module.exports = router;
