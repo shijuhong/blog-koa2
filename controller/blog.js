160e3d543287a150e0ca88062e055a431b88ab89
shijuhong <940104683@qq.com>
2022-06-27 22:36:00 +0800

    实现 MongoDB 存储数据

--- controller/blog.js
+++ controller/blog.js
@@ -1,40 +1,62 @@
 const { exec } = require("../db/mysql");
 const xss = require("xss");
+const Blog = require("../models/Blog");
 
 const getList = async (author, keyword) => {
-  let sql = `select id, title, content, createTime, author from blogs where 1 = 1`;
-  if (author) {
-    sql += ` and author = '${author}'`;
-  }
-  if (keyword) {
-    sql += ` and title like '%${keyword}%'`;
-  }
-  sql += ` order by createTime desc;`;
+  // let sql = `select id, title, content, createTime, author from blogs where 1 = 1`;
+  // if (author) {
+  //   sql += ` and author = '${author}'`;
+  // }
+  // if (keyword) {
+  //   sql += ` and title like '%${keyword}%'`;
+  // }
+  // sql += ` order by createTime desc;`;
 
-  return await exec(sql);
+  // return await exec(sql);
+
+  // 动态拼接查询条件
+  const whereOpt = {};
+  if (author) whereOpt.author = author;
+  if (keyword) whereOpt.keyword = new RegExp(keyword);
+
+  const list = await Blog.find(whereOpt).sort({ _id: -1 });
+  return list;
 };
 
 const getDetail = async (id) => {
-  const sql = `select id, title, content, createTime, author from blogs where id = '${id}'`;
-  const rows = await exec(sql);
-  return rows[0];
+  // const sql = `select id, title, content, createTime, author from blogs where id = '${id}'`;
+  // const rows = await exec(sql);
+  // return rows[0];
+
+  const blog = await Blog.findById(id);
+  return blog;
 };
 
 const newBlog = async (blogData = {}) => {
-  // blogData 是一个博客对象，包含 title、content、author 属性
-  blogData = {
-    ...blogData,
-    createTime: Date.now(),
-    id: 3, // 表示新建博客，插入到数据表里面的 id
-  };
-  const { title, content, author, createTime } = blogData;
-  const sql = `
-    insert into blogs (title, content, createTime, author) 
-    values 
-    ('${xss(title)}', '${xss(content)}', ${createTime}, '${author}');`;
-  const insertData = await exec(sql);
+  // // blogData 是一个博客对象，包含 title、content、author 属性
+  // blogData = {
+  //   ...blogData,
+  //   createTime: Date.now(),
+  // };
+  // const { title, content, author, createTime } = blogData;
+  // const sql = `
+  //   insert into blogs (title, content, createTime, author)
+  //   values
+  //   ('${xss(title)}', '${xss(content)}', ${createTime}, '${author}');`;
+  // const insertData = await exec(sql);
+  // return {
+  //   id: insertData.insertId,
+  // };
+
+  const { title, content, author } = blogData;
+  const blog = await Blog.create({
+    title: xss(title),
+    content: xss(content),
+    author,
+  });
+
   return {
-    id: insertData.insertId,
+    id: blog._id,
   };
 };
 
@@ -45,15 +67,26 @@ const newBlog = async (blogData = {}) => {
  * @returns promise, 包含成功或失败提示信息
  */
 const updateBlog = async (id, blogData = {}) => {
+  // const { title, content } = blogData;
+  // const sql = `
+  //   update blogs set
+  //   title = '${xss(title)}', content = '${xss(content)}'
+  //   where
+  //   id = ${id}
+  // `;
+  // const updatedData = await exec(sql);
+  // return updatedData.affectedRows > 0;
+
   const { title, content } = blogData;
-  const sql = `
-    update blogs set 
-    title = '${xss(title)}', content = '${xss(content)}'
-    where
-    id = ${id}
-  `;
-  const updatedData = await exec(sql);
-  return updatedData.affectedRows > 0;
+  const blog = await Blog.findOneAndUpdate(
+    { _id: id },
+    { title: xss(title), content: xss(content) },
+    // 返回最新内容
+    { new: true }
+  );
+
+  if (blog == null) return false;
+  return true;
 };
 
 /**
@@ -63,12 +96,19 @@ const updateBlog = async (id, blogData = {}) => {
  * @returns boolean 删除成功
  */
 const delBlog = async (id, author) => {
-  const sql = `
-    delete from blogs 
-    where id = '${id}' and author = '${author}';
-  `;
-  const delData = await exec(sql);
-  return delData.affectedRows > 0;
+  // const sql = `
+  //   delete from blogs
+  //   where id = '${id}' and author = '${author}';
+  // `;
+  // const delData = await exec(sql);
+  // return delData.affectedRows > 0;
+
+  const blog = await Blog.findOneAndDelete({
+    _id: id,
+    author,
+  });
+  if (blog == null) return false;
+  return true;
 };
 
 module.exports = {
--- controller/user.js
+++ controller/user.js
@@ -1,18 +1,33 @@
 const { exec, escape } = require("../db/mysql");
+const User = require("../models/User");
 const { genPassword } = require("../utils/cryp");
 
+// const login = async (username, password) => {
+//   username = escape(username);
+//   // 生成加密密码
+//   password = genPassword(password);
+//   password = escape(password);
+
+//   const sql = `
+//     select username, realname from users where
+//     username = ${username} and password = ${password};
+//   `;
+//   const rows = await exec(sql)
+//   return rows[0] || {}
+// };
+
 const login = async (username, password) => {
-  username = escape(username);
   // 生成加密密码
   password = genPassword(password);
-  password = escape(password);
 
-  const sql = `
-    select username, realname from users where
-    username = ${username} and password = ${password};
-  `;
-  const rows = await exec(sql)
-  return rows[0] || {}
+  // find 返回的是数组
+  const userList = await User.find({
+    username,
+    password,
+  });
+
+  if (userList === 0) return {};
+  return userList[0];
 };
 
 module.exports = {
